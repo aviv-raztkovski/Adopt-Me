1f5451cc23e6089bc60b23e86c0a08e5ea7807a5
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2021-10-05 18:29:41 +0300

    made api requests in https instead of http

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -19,7 +19,7 @@ export default function useBreedList(animal) {
             setBreedList([]);
             setStatus("loading");
             const response = await fetch(
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+                `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
             );
 
             const json = await response.json();
